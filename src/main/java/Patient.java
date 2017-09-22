e6383df9d31cc2f3b9d01361a9f439717fdcfc2c
Stanislaw Rogozhin <st.rogozhin@gmail.com>
2017-09-23 00:23:48 +0500

    java files

---
+++ src/main/java/Patient.java
@@ -0,0 +1,26 @@
+public class Patient extends User {
+
+    /**
+     * Строка типа пользователя
+     */
+    private final static String STRING_ROLE_OF_USER = "Patient";
+    /**
+     * Полное имя пациента
+     */
+    private String fullName;
+    /**
+     * Дата рождения пациента
+     */
+    private String dateOfBirthday;
+    /**
+     * Номер телефона пациента
+     */
+    private int phoneNumber;
+
+    public Patient(String login, String password, String fullName, String dateOfBirthday, int phoneNumber){
+        super(login,password);
+        this.fullName = fullName;
+        this.dateOfBirthday = dateOfBirthday;
+
+    }
+}
---
+++ src/main/java/User.java
@@ -0,0 +1,50 @@
+/**
+ * Класс, реализующий пользователя
+ */
+public class User {
+
+    /**
+     * логин пользователя
+     */
+    private String login;
+
+    /**
+     * Пароль пользователя
+     */
+    private String password;
+
+    /**
+     * Установить логин пользователя
+     */
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    /**
+     * установить пароль пользователя
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public User(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    /**
+     * Получить логин пользователя
+     * @return логин
+     */
+    public String getLogin() {
+        return login;
+    }
+
+    /**
+     * Получить пароль пользователя
+     * @return пароль
+     */
+    public String getPassword() {
+        return password;
+    }
+}
